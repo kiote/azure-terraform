96d179abb257e8d79ea5e0c7ce0789ea9cd70539
Ёkaterina Krivich <krivich.ekaterina@gmail.com>
2025-05-23 12:01:09 +0300

    Use devcontainer for setup (#6)

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,13 @@ resource "azurerm_resource_group" "longlegs" {
 }
 
 module "keyvault" {
-  source              = "./modules/keyvault"
-  resource_group_name = azurerm_resource_group.longlegs.name
-  location            = azurerm_resource_group.longlegs.location
-  resource_prefix     = var.resource_prefix
+  source               = "./modules/keyvault"
+  resource_group_name  = azurerm_resource_group.longlegs.name
+  location             = azurerm_resource_group.longlegs.location
+  resource_prefix      = var.resource_prefix
   path_to_license_file = var.path_to_license_file
-  postgres_password   = var.postgres_password
-  common_tags         = var.common_tags
+  postgres_password    = var.postgres_password
+  common_tags          = var.common_tags
 }
 
 module "network" {
@@ -24,24 +24,24 @@ module "network" {
 }
 
 module "compute" {
-  source              = "./modules/compute"
-  resource_group_name = azurerm_resource_group.longlegs.name
-  location            = azurerm_resource_group.longlegs.location
-  resource_prefix     = var.resource_prefix
+  source               = "./modules/compute"
+  resource_group_name  = azurerm_resource_group.longlegs.name
+  location             = azurerm_resource_group.longlegs.location
+  resource_prefix      = var.resource_prefix
   network_interface_id = module.network.network_interface_id
-  ansible_user        = var.ansible_user
-  key_vault_id        = module.keyvault.key_vault_id
-  common_tags         = var.common_tags
+  ansible_user         = var.ansible_user
+  key_vault_id         = module.keyvault.key_vault_id
+  common_tags          = var.common_tags
 }
 
 module "database" {
-  source                = "./modules/database"
-  resource_group_name   = azurerm_resource_group.longlegs.name
-  location              = azurerm_resource_group.longlegs.location
-  resource_prefix       = var.resource_prefix
-  virtual_network_name  = module.network.virtual_network_name
+  source                 = "./modules/database"
+  resource_group_name    = azurerm_resource_group.longlegs.name
+  location               = azurerm_resource_group.longlegs.location
+  resource_prefix        = var.resource_prefix
+  virtual_network_name   = module.network.virtual_network_name
   kubernetes_subnet_cidr = var.kubernetes_subnet_cidr
-  vm_public_ip          = module.network.public_ip
-  postgres_password     = var.postgres_password
-  common_tags           = var.common_tags
+  vm_public_ip           = module.network.public_ip
+  postgres_password      = var.postgres_password
+  common_tags            = var.common_tags
 }
